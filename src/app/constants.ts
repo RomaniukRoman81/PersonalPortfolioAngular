63f9f39d237fca6c8e2c73fddb30d67e9b17b435
roman romaniuk <roman.romanyuk.81@gmail.com>
2020-02-26 12:45:08 +0200

    PPA-2602 changed constant file

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -8,11 +8,19 @@ export class Constants {
         myLinkedin: 'https://www.linkedin.com/in/roman-romaniuk-732685156/',
         myCV: 'https://docs.google.com/document/d/15hqewQ0S5gJYv7Dcxq4CyoCxBuPEb1EqpXj4hnJW5ZM/edit'
     };
-}
 
+    apiRoutes: IApiRoutes = {
+        skillsListUrl: 'http://localhost:4200/assets/skillsData.json',
+        educationalsListUrl: 'http://localhost:4200/assets/educationalsData.json'
+    };
+}
 export interface ISocialMediaLinks {
     myFacebook: string;
     myGithub: string;
     myLinkedin: string;
     myCV: string;
+}
+export interface IApiRoutes {
+    skillsListUrl: string;
+    educationalsListUrl: string;
 }
